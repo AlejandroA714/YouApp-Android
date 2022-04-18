3be93f07d3b74b046684aafae64c1629a167b9d9
Matgaitan <matthew.ramos@novatechdev.com>
2022-04-18 11:41:09 -0600

    Splash y login

---
+++ app/src/main/java/sv/com/udb/youapp/MainActivity.java
@@ -0,0 +1,54 @@
+package sv.com.udb.youapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 5000;
+
+    //Hooks
+    View logo;
+    TextView title1, title2;
+
+    //Animations
+    Animation topAnimation, bottomAnimation, middleAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        middleAnimation = AnimationUtils.loadAnimation(this, R.anim.middle_animation);
+
+        logo = findViewById(R.id.logo);
+        title1 = findViewById(R.id.title1);
+        title2 = findViewById(R.id.title2);
+
+        logo.setAnimation(topAnimation);
+        title1.setAnimation(middleAnimation);
+        title2.setAnimation(bottomAnimation);
+
+        //Splash Screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
