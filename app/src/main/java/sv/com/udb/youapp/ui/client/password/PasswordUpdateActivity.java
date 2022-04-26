e3167b03376c7dc9c9ed775b2ac32204f9d2bb2a
Matgaitan <matthew.ramos@novatechdev.com>
2022-04-26 10:36:26 -0600

    Home finalizado

--- app/src/main/java/sv/com/udb/youapp/ui/client/password/PasswordUpdateActivity.java
+++ app/src/main/java/sv/com/udb/youapp/ui/client/password/PasswordUpdateActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import sv.com.udb.youapp.R;
 import sv.com.udb.youapp.ui.LoginActivity;
@@ -16,6 +18,9 @@ public class PasswordUpdateActivity extends AppCompatActivity {
 
     private ActivityPasswordUpdateBinding binding;
 
+    //Animation
+    Animation animation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,15 @@ public class PasswordUpdateActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.btnAccder.setOnClickListener(this::btnLoginListener);
+
+        //Animation
+        animation = AnimationUtils.loadAnimation(this, R.anim.side_anim);
+
+        //binding.title1.setAnimation(animation);
+        //binding.image.setAnimation(animation);
+        //binding.slogan.setAnimation(animation);
+        //binding.btnAccder.setAnimation(animation);
+        binding.update.setAnimation(animation);
 
     }
 
