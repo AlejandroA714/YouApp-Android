d178f880306fe61382c40c690fbb843976dadb8a
Alejandro1407 <alejandroalejo714@gmail.com>
2022-05-11 12:30:51 -0600

    [Player] - ActivityPlayer, svgs

---
+++ app/src/main/java/sv/com/udb/youapp/ui/client/player/PlayerActivity.java
@@ -0,0 +1,16 @@
+package sv.com.udb.youapp.ui.client.player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sv.com.udb.youapp.R;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
